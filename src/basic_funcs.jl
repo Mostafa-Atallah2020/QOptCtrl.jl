96657d14ef7eb95d272c44e5b7d4bdcd6b24030a
Siddharth Bisht <siddharthbisht97@gmail.com>
2021-10-01 17:41:46 +0530

    define general hamiltonian (#3)

--- src/basic_funcs.jl
+++ src/basic_funcs.jl
@@ -1,10 +1,7 @@
 ket_0 = [1;0]
 ket_1 = [0;1]
 
-pauli_I = [1 0;0 1]
-pauli_X = [0 1;1 0]
-pauli_Y = [0 -1im;1im 0]
-pauli_Z = [1 0;0 -1]
+
 
 function commutator(mat1, mat2)
     mat1 * mat2 - mat2 * mat1
@@ -20,4 +17,12 @@ end
 
 function tr_norm(mat)
     real(tr(sqrt(mat*mat')))
+end
+
+
+function init_pauli(dim)
+    pauli_I = [1 0;0 1]
+    pauli_X = [0 1;1 0]
+    pauli_Y = [0 -1im;1im 0]
+    pauli_Z = [1 0;0 -1]
 end
